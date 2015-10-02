87a997fbcdcbe01d6a624451e95bf66a861a9f50
Ilya Kogan <ikogan@flarecode.com>
2015-10-01 22:16:33 -0400

    Fix up some titles and things.

--- juniper-vpn-connect.sh
+++ juniper-vpn-connect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SSL VPN Script based on openconnect
+# Juniper VPN Script based on openconnect
 #
 # The following packages are required for this to work:
 # gksu zenity libssl-dev libxml2-dev vpnc-scripts
@@ -18,12 +18,12 @@ if [[ "${1}" = "start" ]]; then
 	PID=$(ps aux | grep ${VPN_URL} | grep -v grep | awk '{ print $2 }')
 
 	if [[ ! -z "${PID}" ]]; then
-		zenity --error --title="SSL VPN" --text="SSL VPN already connected."
+		zenity --error --title="Juniper VPN" --text="VPN already connected."
 		exit 1
 	fi
 
 	if [[ ! "$(whoami)" = "root" ]]; then
-		gksu -D "SSL VPN Connection" -u root "${0} ${1} $(whoami)"
+		gksu -D "VPN Connection" -u root "${0} ${1} $(whoami)"
 	fi
 
 	if [[ -z "${2}" ]]; then
@@ -33,12 +33,12 @@ if [[ "${1}" = "start" ]]; then
 	export LD_LIBRARY_PATH=/usr/local/lib
 
 	PROCESS_USERNAME="${2}"
-	DIALOG_OUTPUT=$(zenity --password --username --title="SSL VPN" --ok-label=Connect)
+	DIALOG_OUTPUT=$(zenity --password --username --title="Juniper VPN" --ok-label=Connect)
 	DIALOG_OUTPUT=($(echo "${DIALOG_OUTPUT}" | tr '|' '\n'))
 	LOGIN_USERNAME=${DIALOG_OUTPUT[0]}
 	LOGIN_PASSWORD=${DIALOG_OUTPUT[1]}
 
-	yes | zenity --progress --no-cancel --pulsate --title="SSL VPN" --width 350 --text="Connecting..." --auto-close &>/dev/null &
+	yes | zenity --progress --no-cancel --pulsate --title="Juniper VPN" --width 350 --text="Connecting..." --auto-close &>/dev/null &
 	PROGRESS_PID=$!
 
 	echo ${LOGIN_PASSWORD} | /usr/local/sbin/openconnect --juniper "${VPN_URL}" -U "${PROCESS_USERNAME}" -b -l --timestamp -u "${LOGIN_USERNAME}" --no-cert-check --passwd-on-stdin &>/dev/null
@@ -47,9 +47,9 @@ if [[ "${1}" = "start" ]]; then
 	kill ${PROGRESS_PID} 2>/dev/null
 
 	if [[ ! "${RESULT}" = "0" ]]; then
-		zenity --error --title="SSL VPN" --text="Unable to connect. Check syslog for details." &>/dev/null
+		zenity --error --title="Juniper VPN" --text="Unable to connect. Check syslog for details." &>/dev/null
 	else
-		zenity --info --title="SSL VPN" --text="Connection complete." &>/dev/null
+		zenity --info --title="Juniper VPN" --text="Connection complete." &>/dev/null
 	fi
 elif [[ "${1}" = "stop" ]]; then
 	PID=$(ps aux | grep ${VPN_URL} | grep -v grep | awk '{ print $2 }')
@@ -57,6 +57,6 @@ elif [[ "${1}" = "stop" ]]; then
 	if [[ ! -z "${PID}" ]]; then
 		kill ${PID}
 	else
-		zenity --error --title="SSL VPN" --text="SSL VPN is not running."
+		zenity --error --title="Juniper VPN" --text="VPN is not running."
 	fi
 fi
